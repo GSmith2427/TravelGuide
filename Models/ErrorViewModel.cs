d8495adca995b6cbfad1b8999fec5ae863a4d7b4
Gabriel Smith <gabrielsmith875@gmail.com>
2023-06-08 09:57:26 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelGuideAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
